346b7feff5092e2546d42e74a9ee7c4fa1f33daa
jrfep <jr.ferrer.paris@gmail.com>
2019-02-14 15:59:36 -0500

    Matriz apellidos por estado

--- R/inc00_bajarDatosCNE.R
+++ R/inc00_bajarDatosCNE.R
@@ -5,6 +5,7 @@ require(ROpenOffice)
 ##http://www.cne.gov.ve/web/registro_electoral_descarga/abril2012/nacional.php
 
 data.dir <- "../data/CNE"
+Rdata.dir <- "../Rdata"
 dst.file <- sprintf("%s/nacional.zip",data.dir)
 if (!file.exists(dst.file))
     download.file("http://www.cne.gov.ve/web/registro_electoral_descarga/abril2012/00/nacional.zip", dst.file)
@@ -16,11 +17,34 @@ if (!file.exists(dst.file))
 
 
 
-z <- read.ods(sprintf("%s/ListaParroquiasCNE.ods",data.dir))
 
-Z <- merge(merge(z[[1]],z[[3]],by="edo"),z[[2]],by=c("edo","mun"))
-
-
-x <- read.csv(unz("../data/CNE/nacional.zip","nacional.csv"),as.is=T)
+## registro electoral nacional
+x <- read.csv(unz("../data/CNE/nacional.zip","nacional.csv"),as.is=T,sep=";")
+## ubicación de los centros (codigos)
 y <- read.csv(unz("../data/CNE/centros.zip","centros.csv"),as.is=T,sep=";")
 
+## tabla con la información geográfica [Elaboración propia]
+##(nombre de estados, municipios y parroquias) 
+z <- read.ods(sprintf("%s/ListaParroquiasCNE.ods",data.dir))
+Z <- merge(merge(z[[1]],z[[3]],by="edo"),z[[2]],by=c("edo","mun"))
+
+## Agregar informacion de estados a la tabla del registro electoral
+x$estado <- y$cod_estado[match(x$cod_centro,y$cod_centro)]
+
+## Reformatear información de apellidos en casos en que está desordenada la información original
+x$a1 <- sapply(x$primer_apellido,function(x) strsplit(x," ")[[1]][1])
+x$a2 <- sapply(x$primer_apellido,function(x) strsplit(x," ")[[1]][2])
+x$a2[!(x$segundo_apellido %in% "")] <- x$segundo_apellido[!(x$segundo_apellido %in% "")]
+
+## vectores de apellidos y estados
+apellidos <- gsub("^DE ","",c(x$a1,x$a2))
+estados <- z[[1]]$estado[match(c(x$estado,x$estado),z[[1]]$edo)]
+
+## descartar datos de embajadas, apellido abreviados, vacíos o incompletos
+ss <- (nchar(apellidos) %in% 0:1) |(nchar(apellidos)==2 & grepl("\\.",apellidos)) | apellidos %in% "" | estados %in% "EMBAJADA" 
+table(ss)
+apellidos <- apellidos[!ss]
+estados <- estados[!ss]
+
+mtz.ap.vzla <- table(apellidos,as.character(estados))
+save(file=sprintf("%s/MatrizApellidosVenezuela.rda",Rdata.dir),mtz.ap.vzla)
